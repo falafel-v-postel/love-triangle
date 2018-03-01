143cfb1797ce1477a57b2b13c9a0f82c41f412a5
falafel-v-postel <zztopgrodno@gmail.com>
2018-03-01 16:01:24 +0300

    getLoveTrianglesCount function

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var sum=0;
+  for (var i=0, length=preferences.length; i<length; i++) {
+    const condition1=preferences[preferences[preferences[i]-1]-1]===i+1;
+    const condition2=i+1!==preferences[i];
+    if (condition1&&condition2) {
+      sum++;
+    }
+  }
+  return sum/3;
 };
